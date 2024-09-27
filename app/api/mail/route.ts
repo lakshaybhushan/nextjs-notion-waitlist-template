33489d754c2620488d379ee459bafce826edb6c4
Kayla Kennedy <kaylanicole@Kaylas-Air.attlocal.net>
2024-09-27 17:03:25 -0500

    i dreamer 1  Please enter the commit message for your changes. Lines starting

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -39,10 +39,10 @@ export async function POST(request: NextRequest, response: NextResponse) {
   const { email, firstname } = await request.json();
 
   const { data, error } = await resend.emails.send({
-    from: "Lakshay<hello@waitlist.lakshb.dev>",
+    from: "Tylers<hearmeclearly.store>",
     to: [email],
     subject: "Thankyou for wailisting the Next.js + Notion CMS template!",
-    reply_to: "lakshb.work@gmail.com",
+    reply_to: "hearmeclearly.store",
     html: render(WelcomeTemplate({ userFirstname: firstname })),
   });
 
